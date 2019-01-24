552d09e5ad13607a9d390d18c1fb895459383710
unknown <contrbas@gmail.com>
2019-01-24 12:13:52 +0200

    TIM4 works

--- i2c/main.c
+++ i2c/main.c
@@ -9,6 +9,15 @@
 
 char string[6] = { 0x0 }; /* may be used like extern variable */
 int   i = 0; /* may be used like extern variable */
+#define TIM4_PERIOD       124
+
+INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, 23)
+{
+   printf("We have a timer\n");
+  /* Cleat Interrupt Pending bit */
+  TIM4_ClearITPendingBit(TIM4_IT_UPDATE);
+
+}
 
 #if defined(STM8S105) || defined(STM8S005) ||  defined (STM8AF626x)
 
@@ -112,11 +121,37 @@ static void UART_Config(void)
     enableInterrupts();    
 }
 
+static void TIM4_Config(void)
+{
+  /* TIM4 configuration:
+   - TIM4CLK is set to 16 MHz, the TIM4 Prescaler is equal to 128 so the TIM1 counter
+   clock used is 16 MHz / 128 = 125 000 Hz
+  - With 125 000 Hz we can generate time base:
+      max time base is 2.048 ms if TIM4_PERIOD = 255 --> (255 + 1) / 125000 = 2.048 ms
+      min time base is 0.016 ms if TIM4_PERIOD = 1   --> (  1 + 1) / 125000 = 0.016 ms
+  - In this example we need to generate a time base equal to 1 ms
+   so TIM4_PERIOD = (0.001 * 125000 - 1) = 124 */
+
+  /* Time base configuration */
+  TIM4_TimeBaseInit(TIM4_PRESCALER_128, TIM4_PERIOD);
+  /* Clear TIM4 update flag */
+  TIM4_ClearFlag(TIM4_FLAG_UPDATE);
+  /* Enable update interrupt */
+  TIM4_ITConfig(TIM4_IT_UPDATE, ENABLE);
+  
+  /* enable interrupts */
+  enableInterrupts();
+
+  /* Enable TIM4 */
+  TIM4_Cmd(ENABLE);
+}
+
 void main( void )
 {
   CLK_Config();
   I2C_ACC_Init();
   UART_Config();  
+  TIM4_Config();
   
   if (availability())
   {
