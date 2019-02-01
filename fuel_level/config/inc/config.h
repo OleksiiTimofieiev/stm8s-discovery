d08c484a9d2bf3c2f4ea5b152374147d7305c63a
unknown <contrbas@gmail.com>
2019-02-01 14:16:14 +0200

    working on logic

--- fuel_level/config/inc/config.h
+++ fuel_level/config/inc/config.h
@@ -3,20 +3,21 @@
 
 #include "stm8s_uart2.h"
 
-#define DATA_BUFFER_SIZE         9
+/* timer configs */
 #define TIM4_PERIOD              124
 #define TIMER_PERIOD             3000 /* 1000 == 1 second */
+
+/* buffer defines */
+#define DATA_BUFFER_SIZE         9
 #define REQUEST_6_REPLY_LENGTH   9
 
 /* packet check section */
 #define PACKET_PREFIX            0x3E
 #define PACKET_NET_ADDRESS       1
 #define PACKET_CODE_OPERATION    0x06
-#define PACKET_CONTROL_SUM       0x02
 
-
-void CLK_Config(void);
-void UART2_Config(void);
-void TIM4_Config_Fuel(void);
+void    CLK_Config(void);
+void    UART2_Config(void);
+void    TIM4_Config_Fuel(void);
 
 #endif
