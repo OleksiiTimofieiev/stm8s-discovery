f79675e572cd659b2a74a388977ece6dbd0de29e
unknown <contrbas@gmail.com>
2019-02-04 12:29:28 +0200

    temeperature read

--- fuel_level/main.c
+++ fuel_level/main.c
@@ -4,7 +4,6 @@
 #include "interrupts.h"
 #include "infrastructure.h" /* supporting funcs */
 
-// TODO: handle error with 22 + packet
 // TODO: parse to the structure, 2 bytes
 // TODO: GET_DATA_PARAMS with bitwise operations
 
@@ -26,17 +25,24 @@ t_REQUEST_6_response get_data(uint8_t *data_buffer)
 {
   t_REQUEST_6_response  data;
   
-  data.temperature = 0;
+  data.temperature = data_buffer[3];
   data.relative_level = 0;
   data.frequency = 0;
   
   return (data);
 }
 
+void    zero(t_REQUEST_6_response *data)
+{
+  data->temperature = 0;
+  data->relative_level = 0;
+  data->frequency = 0;
+}
+
 bool    packet_validation(uint8_t *data_buffer)
 {
-  //uint8_t x = crc8(data_buffer, 8);
-  //putchar_UART(x);
+//  uint8_t x = crc8(data_buffer, 8);
+//  putchar_UART(x);
   
   if 
     (
@@ -51,9 +57,13 @@ bool    packet_validation(uint8_t *data_buffer)
 
 void    logic(void)
 {
-  if (received_full_packet)
+  t_REQUEST_6_response data;
+  
+  if (received_full_packet) /* add data processing routines */
   {
 //       putchar_UART('a');
+    zero(&data);
+    
       received_full_packet = FALSE;
       if (packet_validation(data_buffer))
         print_UART(data_buffer); /* handle information */
@@ -64,18 +74,27 @@ void    logic(void)
   {
 //       putchar_UART('b');
       if (packet_validation(data_buffer))
+      {
         print_UART(data_buffer); /* handle information */
+        
+        zero(&data);
+        
+        data = get_data(data_buffer);
+        
+        putchar_UART(data.temperature);
+      }
       buffer_iterator = 0;
       memset(data_buffer, 0x0, sizeof(data_buffer));
   } 
-  else if (byte_received == FALSE)
+  /* else if (byte_received == FALSE)
   {
 //    putchar_UART('c');
     buffer_iterator = 0;
     memset(data_buffer, 0x0, sizeof(data_buffer));
     
-    /* for (int i = 0; i < 100000; i++); */
+     for (int i = 0; i < 100000; i++); 
   }
+  */
 }
 
 void main( void )
